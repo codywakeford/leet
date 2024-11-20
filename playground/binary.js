0c6a141c6a373771078bdada2a704a5aaef9bd44
Cody <codypwakeford@gmail.com>
2024-11-20 21:03:59 +0000

    minor playing

---
+++ playground/binary.js
@@ -0,0 +1,27 @@
+const n = 101001111000101
+
+n
+
+let binary = n.toString(2)
+
+binary
+
+const reversed = binary.split("").reverse().join("")
+reversed
+
+let number = parseInt(reversed, 2)
+number
+
+
+const n2 = 1101
+let p = parseInt(n2, 2)
+p
+
+
+const testing = 10 // 2
+const r = parseInt(testing, 2)
+
+const testing2 = 
+
+
+
